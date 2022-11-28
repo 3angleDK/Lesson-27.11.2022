e4f16b107a5a27039c8451db2510e1474fa1f5d8
Verwolff <3angle@mail.ru>
2022-11-28 15:08:50 +0700

    Task 004. Находим max из 3х чисел.

---
+++ Task 004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа вывода максимального из 3х чисел
+
+Console.Write("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine($"Максимальное из введенных чисел: {max}.");
