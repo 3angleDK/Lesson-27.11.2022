11504e4fb8661c434ef0621157ccc0d049deda5c
Verwolff <3angle@mail.ru>
2022-11-28 10:17:06 +0700

    Task 000. Первая программа, полезные конструкции.

---
+++ Task 000/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Результат: " + n * n);
+Console.WriteLine($"Результат: {n * n}");  // Интерполяция строк
+Console.WriteLine("1\n2\n3\n4\n5");        // Демонстрация печати управляющих символово
