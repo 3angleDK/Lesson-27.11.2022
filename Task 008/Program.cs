f07adc4d854ce2970aa24d88bc650ace86752b12
Verwolff <3angle@mail.ru>
2022-11-28 15:35:42 +0700

    Task 008. Вывод четных чисел от 1 до N

---
+++ Task 008/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа вывода всех четных чисел от 1 до N
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int step = 1;
+if (n <= 0) step = -1;
+for (int i = 1; Math.Abs(i) <= Math.Abs(n); i += step)
+    if (i % 2 == 0)
+        Console.Write($"{i} ");
