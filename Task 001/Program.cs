d46137a792266969e85ba2fcde71d1184c8e7cd1
Verwolff <3angle@mail.ru>
2022-11-28 10:46:28 +0700

    Task 001. N1 == N2 * N2 ?

---
+++ Task 001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вводим 2 числа и проверяем, является ли 1-е число квадратом второго
+Console.Clear();
+Console.Write("Введите 1-е число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n1 == n2 * n2)
+    Console.WriteLine($"Число {n1} является квадратом числа {n2}.");
+else
+    Console.WriteLine($"Число {n1} не является квадратом числа {n2}.");
