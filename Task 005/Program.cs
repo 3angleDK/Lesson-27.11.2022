4a514a62d7b27cd5f0170c2ce40fe7ab2feb5bb2
Verwolff <3angle@mail.ru>
2022-11-28 13:08:59 +0700

    Task 05. Выводим целые числа на интервале [N...N]

---
+++ Task 005/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выводим целые числа на интервале [-N...N]
+
+Console.Write("Веедите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = -n; i <= n; i++)
+    Console.Write($"{i} ");
+
+    
