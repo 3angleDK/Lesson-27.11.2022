18ce0a015a47ae1013922f3933ef0a11053bf7af
Verwolff <3angle@mail.ru>
2022-11-28 11:08:00 +0700

    Task 003. Выводит день недели по его номеру.

---
+++ Task 003/Program.cs
@@ -0,0 +1,22 @@
+﻿// Выдать название дня недели по его номеру
+string[] DayOfWeek = new string[7] { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресение"};
+
+bool NumberInInterval(int value, int min, int max)
+{
+    if (value >= min && value <= max)
+        return true;
+    else 
+    {
+        Console.WriteLine($"Ошибка! Число должно находиться в интервале от {min} до {max}!");
+        return false;
+    }   
+}
+
+int n;
+do
+{
+    Console.Write("Веедите номер дня недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+} while (!NumberInInterval(n, 1, 7));
+
+Console.Write($"День недели: {DayOfWeek[n - 1]}.");
