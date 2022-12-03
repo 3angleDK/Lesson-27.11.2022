d367cc5807a4150225a23bca85d45434b1f1b607
Verwolff <3angle@mail.ru>
2022-12-03 20:54:44 +0700

    Task 013

---
+++ Task 013/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вводим число и выводим третью цифру введенног числа
+// либо выводим что третьей цифры нет
+
+Console.Clear();
+
+Console.Write("Веедите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n;
+
+if (n < 100)
+    Console.WriteLine($"Третьей цифры у числа {n} нет.");
+else 
+{
+    while (m > 999) m /= 10;
+    Console.WriteLine($"Третья цифра у числа {n}: {m % 10}.");
+}
