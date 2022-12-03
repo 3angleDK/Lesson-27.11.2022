66edf52e13aa6842a0a30a315fd5e99ca68a9818
Verwolff <3angle@mail.ru>
2022-12-03 20:54:22 +0700

    Task 012

---
+++ Task 012/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вводим 2 числа и проверяем, является ли второе число кратным первому. 
+// Если 2е число не кратно числу 1му, то выводим остаток от деления.
+
+Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n % m == 0)
+    Console.WriteLine("Кратно");
+else
+    Console.WriteLine($"Не кратно, остаток {n % m}");
+
