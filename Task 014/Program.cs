30de95a81d9bf51e7a4d35523fe33d9a8dddf490
Verwolff <3angle@mail.ru>
2022-12-03 20:55:06 +0700

    Task 014

---
+++ Task 014/Program.cs
@@ -0,0 +1,10 @@
+﻿// вводим число и проверяем, кратно ли оно одновременно 7 и 23
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if ((n % 7 == 0) && (n % 23 == 0))
+    Console.WriteLine("Введенное чилсо кратно 7 и 23.");
+else
+    Console.WriteLine("Введенное чилсо НЕ кратно 7 и 23.");
