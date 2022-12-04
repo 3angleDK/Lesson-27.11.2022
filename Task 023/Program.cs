b5b92b1c2d7ba011411e24aecc117bb20b264f15
Verwolff <3angle@mail.ru>
2022-12-04 20:25:52 +0700

    Task 023 Выводим таблицу кубов чисел [1..N]

---
+++ Task 023/Program.cs
@@ -0,0 +1,8 @@
+﻿// Вводим число N и выводим таблицу кубов чисел от 1 до N
+
+Console.Clear();
+Console.Write("Веедите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+    Console.WriteLine($"{i} -> {i * i * i}");
