ab6a7b2fcfa24748a1fdf8690959ca1c86214849
Verwolff <3angle@mail.ru>
2022-12-04 20:23:12 +0700

    Task 022 Выводим таблицу квадратов чисел [1..N]

---
+++ Task 022/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вводим число N и выводим таблицу квадратов чисел от 1 до N
+
+Console.Clear();
+Console.Write("Веедите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+    Console.WriteLine($"{i} -> {i * i}");
+
