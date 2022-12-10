c64af684ccc28b817e6ddbfb2efec051d63c7da3
Verwolff <3angle@mail.ru>
2022-12-10 19:59:09 +0700

    Task 025 Вычисляем A в степении B

---
+++ Task 025/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вычисляем A в степении B
+
+Console.Clear();
+
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int res = 1;
+for (int i = 1; i <= b; i++)
+    res *= a;
+
+Console.WriteLine($"{a} в степени {b} = {res}.");
