d6d37423fcd6d56d4a4c34749eb476d53f22968b
Verwolff <3angle@mail.ru>
2022-12-10 19:33:49 +0700

    Task 024 Выводим сумму чисел от 1 до N

---
+++ Task 024/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вводим число N и выводим сумму чисел от 1 до N
+
+Console.Clear();
+
+Console.Write("Веедите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// вариант решения в лоб
+int sum = 0;
+for (int i = 1; i <= n; i++)
+    sum += i;
+
+Console.WriteLine($"Сумма чисел от 1 до {n} = {sum}.");
+Console.WriteLine();
+
+// Вариант решения по формуле суммы арифметической прогрессии
+sum = n * (n + 1) / 2;
+Console.WriteLine($"Сумма чисел от 1 до {n} = {sum}.");
