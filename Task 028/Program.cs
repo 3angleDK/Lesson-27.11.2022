55257e1a232d8d252bd095adb16e5b0ec9865802
Verwolff <3angle@mail.ru>
2022-12-10 20:20:31 +0700

    Task 028 Выводим произведение чисел от 1 до N

---
+++ Task 028/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вводим число N и выводим произведение чисел от 1 до N
+
+Console.Clear();
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int res = 1;
+for (int i = 2; i <= n; i++)
+    res *= i;
+
+Console.WriteLine($"Произведение чисел от 1 до {n} = {res}.");
