a34f4ffd274c49f76d81cee31dcd1d02e3289e5c
Verwolff <3angle@mail.ru>
2022-12-10 19:44:42 +0700

    Task 026 Выводим количество цифр в числе

---
+++ Task 026/Program.cs
@@ -0,0 +1,19 @@
+﻿// Выводим количество цифр в числе
+
+Console.Clear();
+
+Console.Write("Веедите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// вариант решения в лоб
+int m = n, count = 0;
+while (m > 0)
+{
+    ++count;
+    m /= 10;
+}
+Console.WriteLine($"В числе {n} {count} цифр(а/ы).");
+Console.WriteLine();
+    
+// Вариант решения через десятичный логарифм
+count = Convert.ToInt32(Math.Log10(n));
+Console.WriteLine($"В числе {n} {count} цифр(а/ы).");
