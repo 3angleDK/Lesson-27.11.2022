603213154a55b79cf5447d5043c0555bb42e4156
Verwolff <3angle@mail.ru>
2022-12-10 19:48:31 +0700

    Task 027 Выводим сумму цифр в числе

---
+++ Task 027/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выводим сумму цифр в числе
+
+Console.Clear();
+
+Console.Write("Веедите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n, sum = 0;
+while (m > 0)
+{
+    sum += m % 10;
+    m /= 10;
+}
+Console.WriteLine($"Сумма цифр в числе {n} = {sum}.");
