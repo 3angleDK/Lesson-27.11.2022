70859e9fcb36a0784b8d72ef0ecfdacd755f2714
Verwolff <3angle@mail.ru>
2022-12-10 20:26:49 +0700

    Task 030 Заполняем массив случайными 0 и 1

---
+++ Task 030/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вводим размер массива из заполняем его нулями и единицами в случайном порядке
+
+void PrintArray(int[] arr)
+{   
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+        Console.Write($"{arr[i]}, ");
+    Console.WriteLine($"{arr[arr.Length - 1]}]");
+}
+Console.Clear();
+
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+    array[i] = new Random().Next(0, 2);
+
+Console.WriteLine();
+Console.Write("Массив: ");
+PrintArray(array);
