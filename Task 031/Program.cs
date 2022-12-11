866095a0f46b868c3b0827d3e886a3fd7c4b008d
Verwolff <3angle@mail.ru>
2022-12-11 22:25:14 +0700

    Task 031 Вычисл. сумм отриц и полож чисел массива

---
+++ Task 031/Program.cs
@@ -0,0 +1,32 @@
+﻿// Заполняем массив случайными числами из диапазона [-9, 9]
+// и вычисляем суммы отрицательных и положительных чисел
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+        Console.Write($"{arr[i]}, ");
+    Console.WriteLine($"{arr[arr.Length - 1]}]");
+}
+
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+    array[i] = new Random().Next(-9, 10);
+
+Console.Write("Исходный массив: ");
+PrintArray(array);
+Console.WriteLine();
+
+int SumPositive = 0, SumNegative = 0;
+for (int i = 0; i < n; i++)
+    if (array[i] > 0)
+        SumPositive += array[i];
+    else    
+        SumNegative += array[i];
+
+Console.WriteLine($"Сумма положительных числе: {SumPositive}.");
+Console.WriteLine($"Сумма отрицательных числе: {SumNegative}.");
