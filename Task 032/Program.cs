e2fb65b50a412f6cfc1189fdadadaebbbabf4a99
Verwolff <3angle@mail.ru>
2022-12-11 22:31:24 +0700

    Task 032 Меняем знак элементов массива

---
+++ Task 032/Program.cs
@@ -0,0 +1,26 @@
+﻿// Меняем знак элементов массива
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+        Console.Write($"{arr[i]}, ");
+    Console.WriteLine($"{arr[arr.Length - 1]}]");
+}
+
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+    array[i] = new Random().Next(-99, 100);
+
+Console.Write("Исходный массив: ");
+PrintArray(array);
+
+for (int i = 0; i < n; i++)
+    array[i] = -array[i]; 
+
+Console.Write("Результат      : ");
+PrintArray(array);
