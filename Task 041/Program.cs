bb3a7d8cf00b33a15e785ea09efa4dc122e6465c
Verwolff <3angle@mail.ru>
2022-12-17 22:32:52 +0700

    Task 041 Количество положительных э-ов в массиве

---
+++ Task 041/Program.cs
@@ -0,0 +1,14 @@
+﻿// Подсчитать количество положительных элементов в массиве
+
+Console.Write("Введите элементы массива: ");
+//double[] array = Console.ReadLine().Split(" ").Select(x => double.Parse(x)).ToArray();
+double[] array = Console.ReadLine().Split(" ").Select(x => Convert.ToDouble(x)).ToArray();
+Console.WriteLine($"Массив: {string.Join(" ", array)}");
+int count = 0;
+
+foreach (double element in array)
+{
+    if (element > 0.0) count++;
+}
+
+Console.WriteLine($"Количество положительных элементов в массиве = {count}");
