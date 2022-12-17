2cf3fa3b0562855123676966634da33e47a52eb4
Verwolff <3angle@mail.ru>
2022-12-17 21:55:35 +0700

    Task 042 Перевод числа в двоичную форму

---
+++ Task 042/Program.cs
@@ -0,0 +1,16 @@
+﻿// Перевести число в двоичную форму
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a;
+string bin = String.Empty;
+bin = "";
+
+while (b > 0)
+{
+    bin = Convert.ToString(b % 2) + bin;
+    b /= 2;    
+}
+
+Console.WriteLine($"Число {a} = {bin} в двоичной системе.");
