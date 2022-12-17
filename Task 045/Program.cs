c7a93994a010af25b297179d16afac59f5040cd5
Verwolff <3angle@mail.ru>
2022-12-18 03:24:17 +0700

    Task 045 Поэлементное копирование массива

---
+++ Task 045/Program.cs
@@ -0,0 +1,12 @@
+﻿// Поэлементное копирование массива
+
+Console.Clear();
+Console.Write("Введите элементы массива: ");
+int[] a = Console.ReadLine().Trim().Split(" ").Select(x => Convert.ToInt32(x)).ToArray();
+int[] b = new int[a.Length];
+
+for (int i = 0; i < a.Length; i++)
+    b[i] = a[i];
+
+Console.WriteLine($"Исходный массив: {string.Join(", ", a)}");
+Console.WriteLine($"Конечный массив: {string.Join(", ", b)}");
