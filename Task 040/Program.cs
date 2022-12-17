ea2eee4b6eeb79022c7c5b6b903a5d0af53e41e9
Verwolff <3angle@mail.ru>
2022-12-17 21:54:34 +0700

    Task 040 Существование треугольника

---
+++ Task 040/Program.cs
@@ -0,0 +1,13 @@
+﻿// Проверить существование треугольника по трем сторонам
+Console.Clear();
+Console.Write("Введите длину 1й стороны треуголника (a): ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите длину 2й стороны треуголника (b): ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите длину 3й стороны треуголника (c): ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+if ((a + b > c) && (a + c > b) && (c + b > a))
+    Console.WriteLine("Треугольник существует.");
+else
+    Console.WriteLine("Треугольник НЕ существует.");
