759620bcea4026e7154a14da6132c6faee29f6dc
Verwolff <3angle@mail.ru>
2022-12-18 00:44:31 +0700

    Task 043a Вывести все перестановки символов строки

---
+++ Task 043a/Program.cs
@@ -0,0 +1,27 @@
+﻿// Вывести все перестановки символов строки
+
+void perm(char[] s, int pos)
+{
+    if (pos == s.Length - 1)
+        Console.WriteLine(s);
+    else
+    {
+        perm(s, pos + 1);
+        for (int i = pos + 1; i < s.Length; ++i)
+        {
+            char tmp = s[pos];
+            s[pos] = s[i];
+            s[i] = tmp;
+            perm(s, pos + 1);
+            tmp = s[pos];
+            s[pos] = s[i];
+            s[i] = tmp;
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите строку: ");
+char[] s = Console.ReadLine().ToCharArray();
+
+perm(s, 0);
