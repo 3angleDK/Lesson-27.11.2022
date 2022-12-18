edd1ca53756d096b84bbab4157c6c77c60059f3e
Verwolff <3angle@mail.ru>
2022-12-18 21:53:56 +0700

    Task 046 Заполнить двумерный массив случ. числами

---
+++ Task 046/Program.cs
@@ -0,0 +1,19 @@
+﻿// Создать двумерный массив и заполнить его случайными числами
+void InputMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            matrix[row, col] = new Random().Next(-10, 10);
+            Console.Write($"{matrix[row, col]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
