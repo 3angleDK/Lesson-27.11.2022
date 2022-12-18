46d6106b8f556edf566e2fe576206ee53e357a71
Verwolff <3angle@mail.ru>
2022-12-18 21:56:07 +0700

    Task 048 Заполнить массив по формуле A[m,n] = m+n

---
+++ Task 048/Program.cs
@@ -0,0 +1,19 @@
+﻿// Заполнить двумерный массив по формуле A[m,n] = m+n
+void InputMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            matrix[row, col] = col + row;
+            Console.Write($"{matrix[row, col]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Trim().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
