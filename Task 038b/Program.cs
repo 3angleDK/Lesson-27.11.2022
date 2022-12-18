9563e86396faf410d8526ef6c8ad5e3f80ed7c66
Verwolff <3angle@mail.ru>
2022-12-18 18:19:02 +0700

    Task 038b Гипотеза Гольдбаха

--- Task 038b/Program.cs
+++ Task 038b/Program.cs
@@ -14,7 +14,7 @@ bool IsNumberCorrect(int value)
 
 bool IsNumberPrime(int value)
 {
-    for (int i = 2; i < value; i++)
+    for (int i = 2; i <= value / 2; i++)
         if (value % i == 0)
             return false;
     return true;
