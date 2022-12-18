0b202b6e065fb6e753ccf6ac7a494713adc050f2
Verwolff <3angle@mail.ru>
2022-12-18 22:35:54 +0700

    Task 052a Транспонирование матрицы

---
+++ Task 052a/Program.cs
@@ -0,0 +1,45 @@
+﻿// Транспонирование матрицы
+
+void InputMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+        for (int col = 0; col < matrix.GetLength(1); col++)
+            matrix[row, col] = new Random().Next(-10, 11);
+}
+
+void OutputMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+            Console.Write($"{matrix[row, col]}\t");
+        Console.WriteLine();
+    }
+}
+
+int[,] TransposeMatrix(int[,] matrix)
+{
+    int[,] result = new int[matrix.GetLength(1), matrix.GetLength(0)];
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            result[col, row] = matrix[row, col];
+        }
+    }
+    return result;        
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Trim().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Исходная матрица:");
+OutputMatrix(matrix);
+
+int[,] result = TransposeMatrix(matrix);
+
+Console.WriteLine("Конечная матрица:");
+OutputMatrix(result);
+
