88150f965bab7807173bad635a62193f1c715710
Verwolff <3angle@mail.ru>
2022-12-18 22:11:06 +0700

    Task 051 Сумма эл-в главной диагонали матрицы

---
+++ Task 051/Program.cs
@@ -0,0 +1,36 @@
+﻿// Найти сумму элементов двумерного массива на главной диагонали
+
+void InputMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+        for (int col = 0; col < matrix.GetLength(1); col++)
+            matrix[row, col] = new Random().Next(-10, 11);
+}
+
+void OutputMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+            Console.Write($"{matrix[row, col]}\t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Trim().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Исходная матрица:");
+OutputMatrix(matrix);
+
+int sum = 0;
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    for (int col = 0; col < matrix.GetLength(1); col++)
+        if (col == row)
+            sum += matrix[row,col];
+}
+
+Console.WriteLine($"Сумма элементов на главной диагонали = {sum}.");
