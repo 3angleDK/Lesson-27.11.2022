fc70ba6b2ba4d1a8aa30212f009ec0db9be1df6e
Verwolff <3angle@mail.ru>
2022-12-18 21:58:13 +0700

    Task 049 A[i,j] = A[i,j]^2 если i и j - нечетные

---
+++ Task 049/Program.cs
@@ -0,0 +1,42 @@
+﻿// Заменить в двумерном массиве элементы, у которых оба индекса нечетные,
+// на их квадраты
+
+void InputMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+        for (int col = 0; col < matrix.GetLength(1); col++)
+            matrix[row, col] = new Random().Next(-10, 11);
+}
+
+void OutputMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+            Console.Write($"{matrix[row, col]}\t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Trim().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Исходная матрица:");
+OutputMatrix(matrix);
+
+int[,] result = new int[size[0], size[1]];
+
+for (int row = 0; row < result.GetLength(0); row++)
+{
+    for (int col = 0; col < result.GetLength(1); col++)
+        if (row % 2 == 1 && col % 2 ==1)
+            result[row,col] = matrix[row,col] * matrix[row,col];
+        else
+            result[row,col] = matrix[row,col];
+}
+
+Console.WriteLine("Конечная матрица:");
+OutputMatrix(result);
+
