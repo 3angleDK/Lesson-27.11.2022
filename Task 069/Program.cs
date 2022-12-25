88fce165ef74441b00936061c6ccd11f8411b89d
Verwolff <3angle@mail.ru>
2022-12-25 21:25:13 +0700

    Task 069 Рекурсия. Возвести число A в степень B

---
+++ Task 069/Program.cs
@@ -0,0 +1,15 @@
+﻿// Возвести число A в степень B (с помощью рекурсии)
+
+int Power(int num, int degree)
+{
+    if (degree == 0) return 1;
+    return Power(num, degree-1) * num;
+}
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{a} в степени {b} = {Power(a,b)}");
+
