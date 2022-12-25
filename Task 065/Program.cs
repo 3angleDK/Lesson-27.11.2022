df03808b64f3335cb7acdb8538f768b7c128a0fe
Verwolff <3angle@mail.ru>
2022-12-25 20:46:18 +0700

    Task 065. Рекурсия. Вывод чисел на отрезке [M, N]

---
+++ Task 065/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вывести все натуральные числа на отрезке [M, N] (с помощью рекурсии)
+
+string GetNumbers(int m, int n)
+{
+    if (n == m) return n.ToString();
+    return (m < n) ? GetNumbers(m, n - 1) + $", {n}": 
+                     GetNumbers(m, n + 1) + $", {n}";
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Числа: {GetNumbers(m, n)}");
