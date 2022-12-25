8ce690968819616ef1286a3371d7130247eb2ea2
Verwolff <3angle@mail.ru>
2022-12-25 20:27:18 +0700

    Task 063 Рекурсия. Вывод чисел от 1 до N

---
+++ Task 063/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести все натуральные числа на отрезке [1, N] (с помощью рекурсии)
+
+string GetNumbers(int n)
+{
+    if (n == 1) return "1";
+    return GetNumbers(n-1) + $", {n}";
+}
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Числа: {GetNumbers(n)}");
